a72346290136d67d75bada6e1d5f9051af4e3995
Maciej Glowacki <m.glowacki01@gmail.com>
2021-01-23 22:31:49 +0100

    enabled all

--- config/middleware.js
+++ config/middleware.js
@@ -1,13 +1,8 @@
 module.exports = {
   settings: {
     cors: {
-      origin: [
-        "http://localhost:1337",
-        "https://maciej-dev.netlify.app",
-        "https://www.maciej-dev.netlify.app",
-        "https://nervous-goodall-0e0adc.netlify.app",
-        "https://www.nervous-goodall-0e0adc.netlify.app",
-      ],
+      enabled: true,
+      origin: "*",
     },
   },
 };
